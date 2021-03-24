b59bc887c9e402c853a0b8772f575b69bfca31f1
Juliana <julifs.13@gmail.com>
2021-03-23 21:01:24 -0300

    Estado trancado e taxas funcionando

--- markovchain.py
+++ markovchain.py
@@ -8,13 +8,12 @@ plt.ion()
 
 #TODO
 #criar variável para proporção entre retidos e não retidos - FEITO
-#adicionar estado trancado - FEITO COM ERRO no p>1
-#A1+A1R até E com gráfico
-#tempo até retenção - Não passa por varios estados, faz sentido? talvez com o estado trancado
+#adicionar estado trancado - FEITO
 #extrair formula
 #analise de sobrevivencia/lifelines
+#A1+A1R até E com gráfico
+#tempo até retenção - Não passa por varios estados, faz sentido? talvez com o estado trancado
 #criar versão considerando anos no curso
-
 
 
 taxaRetencao = 1.0
@@ -30,8 +29,8 @@ taxaProporcaoVoltar = 1.0
 
 # Probabilidade dos estados
 # A1
-A1toT = 0 * taxaTrancar
-#A1toT = 0.15 * taxaTrancar
+#A1toT = 0 * taxaTrancar
+A1toT = 0.15 * taxaTrancar
 A1toA1R = 0.3 * taxaRetencao
 A1toE = 0.1 * taxaEvasao
 A1toA2 = 1 - A1toA1R - A1toE - A1toT
@@ -44,8 +43,8 @@ A1RtoA2R = 1 - A1RtoE - A1RtoT
 TtoA1R = 0.1 * taxaVoltar
 
 # A2
-A2toT = 0 * taxaTrancar
-#A2toT = 0.12 * taxaTrancar
+#A2toT = 0 * taxaTrancar
+A2toT = 0.12 * taxaTrancar
 A2toA2R = 0.25 * taxaRetencao
 A2toE = 0.1 * taxaEvasao
 A2toA3 = 1 - A2toA2R - A2toE - A2toT
@@ -58,8 +57,8 @@ A2RtoA3R = 1 - A2RtoE - A2RtoT
 TtoA2R = 0.1 * taxaVoltar
 
 # A3
-A3toT = 0 * taxaTrancar
-#A3toT = 0.8 * taxaTrancar
+#A3toT = 0 * taxaTrancar
+A3toT = 0.08 * taxaTrancar
 A3toA3R = 0.2 * taxaRetencao
 A3toE = 0.1 * taxaEvasao
 A3toA4 = 1 - A3toA3R - A3toE - A3toT
@@ -72,8 +71,8 @@ A3RtoA4R = 1 - A3RtoE - A3RtoT
 TtoA3R = 0.1 * taxaVoltar
 
 # A4
-A4toT = 0 * taxaTrancar
-#A4toT = 0.1 * taxaTrancar
+#A4toT = 0 * taxaTrancar
+A4toT = 0.1 * taxaTrancar
 A4toA4R = 0.15 * taxaRetencao
 A4toE = 0.06 * taxaEvasao
 A4toA5 = 1 - A4toA4R - A4toE - A4toT
@@ -87,7 +86,7 @@ TtoA4R = 0.1 * taxaVoltar
 
 # A5
 A5toT = 0.0 * taxaTrancar
-#A5toT = 0.05 * taxaTrancar
+A5toT = 0.05 * taxaTrancar
 A5toA5R = 0.2 * taxaRetencao
 A5toE = 0.03 * taxaEvasao
 A5toG = 1 - A5toA5R - A5toE - A5toT
@@ -103,39 +102,36 @@ progres = [A1toA2, A2toA3, A3toA4, A4toA5, A5toG, A1RtoA2R, A2RtoA3R, A3RtoA4R, 
 ret = [A1toA1R, A2toA2R, A3toA3R, A4toA4R, A5toA5R]
 tranc = [A1toT, A2toT, A3toT, A4toT, A5toT, A1RtoT, A2RtoT, A3RtoT, A4RtoT, A5RtoT]
 
-# statenames = ['A1', 'A2', 'A3', 'A4', 'A5', 'A1R', 'A2R', 'A3R', 'A4R', 'A5R', 'T', 'G', 'E']
-# state = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-# p = [[0.0, A1toA2, 0.0, 0.0, 0.0, A1toA1R, 0.0, 0.0, 0.0, 0.0, A1toT, 0.0, A1toE],
-#      [0.0, 0.0, A2toA3, 0.0, 0.0, 0.0, A2toA2R, 0.0, 0.0, 0.0, A2toT, 0.0, A2toE],
-#      [0.0, 0.0, 0.0, A3toA4, 0.0, 0.0, 0.0, A3toA3R, 0.0, 0.0, A3toT, 0.0, A3toE],
-#      [0.0, 0.0, 0.0, 0.0, A4toA5, 0.0, 0.0, 0.0, A4toA4R, 0.0, A4toT, 0.0, A4toE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5toA5R, A5toT, A5toG, A5toE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A1RtoA2R, 0.0, 0.0, 0.0, A1RtoT, 0.0, A1RtoE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A2RtoA3R, 0.0, 0.0, A2RtoT, 0.0, A2RtoE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A3RtoA4R, 0.0, A3RtoT, 0.0, A3RtoE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A4RtoA5R, A4RtoT, 0.0, A4RtoE],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5RtoT, A5RtoG, A5RtoE],
-#      [TtoA1, TtoA2, TtoA3, TtoA4, TtoA5, TtoA1R, TtoA2R, TtoA3R, TtoA4R, TtoA5R, 0.0, 0.0, 0.0],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
+statenames = ['A1', 'A2', 'A3', 'A4', 'A5', 'A1R', 'A2R', 'A3R', 'A4R', 'A5R', 'T', 'G', 'E']
+state = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+p = [[0.0, A1toA2, 0.0, 0.0, 0.0, A1toA1R, 0.0, 0.0, 0.0, 0.0, A1toT, 0.0, A1toE],
+     [0.0, 0.0, A2toA3, 0.0, 0.0, 0.0, A2toA2R, 0.0, 0.0, 0.0, A2toT, 0.0, A2toE],
+     [0.0, 0.0, 0.0, A3toA4, 0.0, 0.0, 0.0, A3toA3R, 0.0, 0.0, A3toT, 0.0, A3toE],
+     [0.0, 0.0, 0.0, 0.0, A4toA5, 0.0, 0.0, 0.0, A4toA4R, 0.0, A4toT, 0.0, A4toE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5toA5R, A5toT, A5toG, A5toE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A1RtoA2R, 0.0, 0.0, 0.0, A1RtoT, 0.0, A1RtoE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A2RtoA3R, 0.0, 0.0, A2RtoT, 0.0, A2RtoE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A3RtoA4R, 0.0, A3RtoT, 0.0, A3RtoE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A4RtoA5R, A4RtoT, 0.0, A4RtoE],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5RtoT, A5RtoG, A5RtoE],
+     [TtoA1, TtoA2, TtoA3, TtoA4, TtoA5, TtoA1R, TtoA2R, TtoA3R, TtoA4R, TtoA5R, 0.0, 0.0, 0.0],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
 
-statenames = ['A1', 'A2', 'A3', 'A4', 'A5', 'A1R', 'A2R', 'A3R', 'A4R', 'A5R', 'G', 'E']
-state = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
-p = [[0.0, A1toA2, 0.0, 0.0, 0.0, A1toA1R, 0.0, 0.0, 0.0, 0.0, 0.0, A1toE],
-     [0.0, 0.0, A2toA3, 0.0, 0.0, 0.0, A2toA2R, 0.0, 0.0, 0.0, 0.0, A2toE],
-     [0.0, 0.0, 0.0, A3toA4, 0.0, 0.0, 0.0, A3toA3R, 0.0, 0.0, 0.0, A3toE],
-     [0.0, 0.0, 0.0, 0.0, A4toA5, 0.0, 0.0, 0.0, A4toA4R, 0.0, 0.0, A4toE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5toA5R, A5toG, A5toE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A1RtoA2R, 0.0, 0.0, 0.0, 0.0, A1RtoE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A2RtoA3R, 0.0, 0.0, 0.0, A2RtoE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A3RtoA4R, 0.0, 0.0, A3RtoE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A4RtoA5R, 0.0, A4RtoE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5RtoG, A5RtoE],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-     [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
-
-# statenames = ['Ingressante', 'A1', 'A2', 'A3', 'A4', 'A5', 'A1R', 'A2R', 'A3R', 'A4R', 'A5R', 'Trancado', 'Graduado', 'Evadido']
-# state = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+# statenames = ['A1', 'A2', 'A3', 'A4', 'A5', 'A1R', 'A2R', 'A3R', 'A4R', 'A5R', 'G', 'E']
+# state = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+# p = [[0.0, A1toA2, 0.0, 0.0, 0.0, A1toA1R, 0.0, 0.0, 0.0, 0.0, 0.0, A1toE],
+#      [0.0, 0.0, A2toA3, 0.0, 0.0, 0.0, A2toA2R, 0.0, 0.0, 0.0, 0.0, A2toE],
+#      [0.0, 0.0, 0.0, A3toA4, 0.0, 0.0, 0.0, A3toA3R, 0.0, 0.0, 0.0, A3toE],
+#      [0.0, 0.0, 0.0, 0.0, A4toA5, 0.0, 0.0, 0.0, A4toA4R, 0.0, 0.0, A4toE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5toA5R, A5toG, A5toE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A1RtoA2R, 0.0, 0.0, 0.0, 0.0, A1RtoE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A2RtoA3R, 0.0, 0.0, 0.0, A2RtoE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A3RtoA4R, 0.0, 0.0, A3RtoE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A4RtoA5R, 0.0, A4RtoE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, A5RtoG, A5RtoE],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+#      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
 
 # statenames = ['Y1', 'Y2', 'Y3', 'Y4', 'I', 'G', 'W']
 # state = np.array([[1, 0, 0, 0, 0, 0, 0]])
@@ -147,18 +143,12 @@ p = [[0.0, A1toA2, 0.0, 0.0, 0.0, A1toA1R, 0.0, 0.0, 0.0, 0.0, 0.0, A1toE],
 #      [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
 #      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
 
-# statenames = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'G', 'W']
-# state = np.array([[1, 0, 0, 0, 0, 0, 0, 0]])
-
 # statenames = ['Inicio', 'A1', 'A2', 'A3', 'A4', 'Graduado', 'Evadido']
 # state = np.array([[1, 0, 0, 0, 0, 0, 0]])
 # p = [[0.0, 0.89, 0.09, 0.02, 0.0, 0.0, 0.0], [0.0, 0.17, 0.64, 0.06, 0.0, 0.0, 0.13],
 #      [0.0, 0.0, 0.1, 0.75, 0.08, 0.0, 0.07], [0.0, 0.0, 0.0, 0.08, 0.84, 0.04, 0.04],
 #      [0.0, 0.0, 0.0, 0.0, 0.37, 0.61, 0.02], [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
 #      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]]
-
-# normalize rows to ensure p is a valid right stochastic matrix
-# p = p / np.sum(p, axis=1)
 
 #Desenha Cadeia de Markov
 graph = Graph()
@@ -216,7 +206,11 @@ print("\n Duração esperada em cada ano até a graduação ou evasão")
 print(np.round(mc.absorption_times, 3))
 
 print("\n Duração média esperada até graduação:")
-print(np.round(np.trace(np.asarray(N)), 3))
+# Sem estados retidos
+#print(np.round(np.trace(np.asarray(N)), 3))
+# Com estados retidos
+print(np.round(np.trace((np.asarray(N)/2)), 3))
+
 
 probGE = prob_absor()
 for i in range(len(probGE)):
@@ -253,23 +247,25 @@ dfGE = pd.DataFrame({'Não Retidos': g[:5],
                     'Retidos': g[-5:]}, index=statenames[:5])
 dfGE.plot.bar(rot=0, color={"Não Retidos": "green", "Retidos": "red"}, title="Probabilidade de Graduação")
 
+
 #Gráfico do histórico de distribuição
-for x in range(8):
+for x in range(15):
     # probalidade dos estado
-    # print(np.round(state, 3))
+    #print(np.round(state, 3))
     state = np.dot(state, p)
     stateHist = np.append(stateHist, state, axis=0)
     dfDistrHist = pd.DataFrame(stateHist, columns=statenames)
 
+
 dfDistrHist.plot()
-# print(dfDistrHist)
+#print(dfDistrHist)
 
 
 #lifelines
 # dfDistrHist.insert(0, 'T', [0, 1, 2, 3, 4, 5, 6, 7, 8], True)
 #
 # T = dfDistrHist['T']
-# E = dfDistrHist['E']
+# E = dfDistrHistTotal['E']
 #
 # kmf = KaplanMeierFitter()
 # kmf.fit(T, event_observed=E)
---
+++ test1.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from random import seed
+from random import random
+import matplotlib.pyplot as plt
+P = np.array([[0.057, 0.31, 0.0, 0.0, 0.603, 0.0, 0.03],
+      [0.0, 0.187, 0.528, 0.0, 0.285, 0.0, 0.0],
+      [0.0, 0.0, 0.012, 0.563, 0.097, 0.328, 0.0],
+      [0.0, 0.0, 0.0, 0.039, 0.094, 0.814, 0.053],
+      [0.001, 0.036, 0.02, 0.0, 0.0, 0.003, 0.94],
+      [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
+stateChangeHist= np.array([[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0],
+[0.0,  0.0,  0.0, 0.0, 0.0, 0.0, 0.0]])
+
+state=np.array([[1, 0, 0, 0, 0, 0, 0]])
+currentState=0
+stateHist=state
+dfStateHist=pd.DataFrame(state)
+distr_hist = [[0,0,0,0,0,0,0]]
+seed(4)
+# Simulate from multinomial distribution
+def simulate_multinomial(vmultinomial):
+  r=np.random.uniform(0.0, 1.0)
+  CS=np.cumsum(vmultinomial)
+  CS=np.insert(CS,0,0)
+  m=(np.where(CS<r))[0]
+  nextState=m[len(m)-1]
+  return nextState
+for x in range(1000):
+  currentRow=np.ma.masked_values((P[currentState]), 0.0)
+  nextState=simulate_multinomial(currentRow)
+  # Keep track of state changes
+  stateChangeHist[currentState,nextState]+=1
+  # Keep track of the state vector itself
+  state=np.array([[0,0,0]])
+  state[0,nextState]=1.0
+  # Keep track of state history
+  stateHist=np.append(stateHist,state,axis=0)
+  currentState=nextState
+  # calculate the actual distribution over the 3 states so far
+  totals=np.sum(stateHist,axis=0)
+  gt=np.sum(totals)
+  distrib=totals/gt
+  distrib=np.reshape(distrib,(1,3))
+  distr_hist=np.append(distr_hist,distrib,axis=0)
+print(distrib)
+P_hat=stateChangeHist/stateChangeHist.sum(axis=1)[:,None]
+# Check estimated state transition probabilities based on history so far:
+print(P_hat)
+dfDistrHist = pd.DataFrame(distr_hist)
+# Plot the distribution as the simulation progresses over time
+dfDistrHist.plot(title="Simulation History")
+plt.show()
