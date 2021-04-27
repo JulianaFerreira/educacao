440d61d6e3ebaf99c6582c7ceee58c749b808a77
Juliana <julifs.13@gmail.com>
2021-04-27 16:49:12 -0300

    Experimentos

--- simulation.py
+++ simulation.py
@@ -225,7 +225,7 @@ quantAlunos = 10000
 
 
 # Simulação Geral
-time, event = Simu(quantAlunos, 'matrix/matrixBrezav.csv')
+time, event = Simu(quantAlunos, 'matrix/matrixBairagiAlterado.csv')
 
 sobrevivencia([time], [event], ['Estudantes'], "Análise de Sobrevivência")
 
