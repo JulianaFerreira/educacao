a7912736a6aa6cd372df1d6e93e73b75e1c9bd04
Juliana <julifs.13@gmail.com>
2021-05-18 22:08:37 -0300

    Criando matriz de probabilidades com os dados por ano

--- generate_probabilities.py
+++ generate_probabilities.py
@@ -2,20 +2,58 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv("docs/df_survivability_bsi-bcc.csv")
+# df = pd.read_csv("docs/df_survivability_bsi-bcc.csv")
+# df.info()
+#
+# # duracao_vinculo por ano
+# df['DURACAO_VINCULO'] = round((df['DURACAO_VINCULO']/2)+0.4)
+# df.to_csv("docs/df_survivability_bsi-bcc-ano.csv")
 
-# alterar csv de período para ano
-df_ano = df[df['NM_SEXO'] == 'F']
-df_ano.to_csv("docs/df_survivability_bsi-bcc-ano.csv")
+df = pd.read_csv("docs/df_survivability_bsi-bcc-ano.csv")
+
+# Para todos os estudantes
+# df_todos = df.groupby(['DURACAO_VINCULO', 'STATUS'])['QTD'].sum()
+# df_todos.to_csv("docs/df_survivability_bsi-bcc-ano_todos.csv")
+df_todos = pd.read_csv("docs/df_survivability_bsi-bcc-ano_todos.csv")
 
 
+def transition_matrix(M):
+    #convert to probabilities:
+    for row in M:
+        s = sum(row)
+        if s > 0:
+            row[:] = [f/s for f in row]
+    return M
 
-df_todos = df.groupby(['DURACAO_VINCULO', 'STATUS'])['QTD'].sum()
-df_test = df.groupby(['DURACAO_VINCULO'])['QTD'].sum()
 
-df_todos.to_csv("docs/df_survivability_bsi-bcc_todos.csv")
+#criar tabela com as quantidades
+def quantity_matrix(df, n):
+    M = np.zeros((n,n))
 
+    # Fazer array para e, c e v, para não dar problemas quando tiver valor 0
+    # evadido adiciona na ultima coluna e linha do duracao_vinculo
+    e = df[df['STATUS'] == 'EVADIDO']['QTD'].values
+    M[:, n-1] = e
 
+    # concluido adiciona na penultima coluna e linha do duracao_vinculo
+    c = df[df['STATUS'] == 'CONCLUIDO']['QTD'].values
+    print(c)
+    #M[:, n-2] = c
+
+    # vinculado adiciona coluna do duracao_vinculo
+    v = df[df['STATUS'] == 'VINCULADO']['QTD'].values
+    print(v)
+    for i in range(n):
+        for j in range(n):
+            if j == i+1:
+                M[i, j] = v[i]
+
+    print(M)
+    return M
+
+m = quantity_matrix(df_todos, 11)
+# p = transition_matrix(m)
+# print(p)
 
 
 
@@ -36,34 +74,6 @@ df_todos.to_csv("docs/df_survivability_bsi-bcc_todos.csv")
 
 
 
-
-
-
-# Cria matriz de transição passando um array com os estados transicionando
-def transition_matrix(transitions):
-    n = 1 + max(transitions) #number of states
-
-    M = [[0]*n for _ in range(n)]
-
-    for (i, j) in zip(transitions, transitions[1:]):
-        M[i][j] += 1
-
-    print(M)
-
-    #now convert to probabilities:
-    for row in M:
-        s = sum(row)
-        if s > 0:
-            row[:] = [f/s for f in row]
-    return M
-
-
-
-# estados transicionando
-# t = [1,1,2,6,8,5,5,7,8,8,1,1,4,5,5,0,0,0,1,1,4,4,5,1,3,3,4,5,4,1,1]
-# states = np.unique(t)
-# p = transition_matrix(t)
-# print(p)
 
 
 
